38b4902145562f1d67bdbba2881766090496822f
253-Mathilde <arianebaby08@gmail.com>
2026-08-03 20:54:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -591,19 +591,19 @@ const songs = [
   {
     name: "LEASE",
     singer: "Takeshi Abo",
-    poster: "https://i.postimg.cc/1tkgPNzx/Welcome-to-Peter-s-and-Tinker-s-Playlist-(5).png",
+    poster: "https://files.catbox.moe/t9tyjh.png",
     audio: "https://files.catbox.moe/rdnozd.mp3"
   },
   {
     name: "Dinner is Not Over",
     singer: "Jack Stauber",
-    poster: "https://i.postimg.cc/1tkgPNzx/Welcome-to-Peter-s-and-Tinker-s-Playlist-(5).png",
+    poster: "https://files.catbox.moe/t9tyjh.png",
     audio: "https://files.catbox.moe/nbfxps.mp3"
   },
   {
     name: "Dramatic",
     singer: "Mass of the Fermenting Dregs",
-    poster: "https://i.postimg.cc/Bn3m4MYc/Welcome-to-Peter-s-and-Tinker-s-Playlist-(6).png",
+    poster: "https://files.catbox.moe/2a9yyz.png",
     audio: "https://files.catbox.moe/hvlg1q.mp3"
   }
 ];
