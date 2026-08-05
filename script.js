258b17a6f749051dba6ec8c1e983653cbf31a226
253-Mathilde <arianebaby08@gmail.com>
2026-08-05 19:15:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -118,6 +118,9 @@ hubbleClose.addEventListener("click", function() {
   closeWindow(hubbleWindow);
 });
 
+hubbleOpen.addEventListener("click", function() {
+ window.open("https://science.nasa.gov/specials/apps/what-did-hubble-see-on-your-birthday/", "_blank");
+});
 
 document.querySelector(".iconhubble").addEventListener("dblclick", () => {
 window.open("https://science.nasa.gov/specials/apps/what-did-hubble-see-on-your-birthday/", "_blank");
